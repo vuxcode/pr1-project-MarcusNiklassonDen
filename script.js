aac92b212918a02e1ea1f578e64bd754ba6eea2e
Marcus Niklasson <85846673+MarcusNiklassonDen@users.noreply.github.com>
2023-11-07 19:25:40 +0100

    new event listeners

--- script.js
+++ script.js
@@ -5,6 +5,38 @@ canvas.height = window.innerHeight; //sets canvas height to window size
 
 var ctx = canvas.getContext("2d"); // dont really know what this dose but i guess it sets var ctx to be a 2d space?
 
+ctx.strokeStyle = "black"; // sets color of stroke
+ctx.lineCap = "round" //sets end to be round
+ctx.lineWidth = 10; //sets width of stroke
+
+var isDrawing = false; // sets variable isDrawning to boolian false
+var X = 0; //sets x value to undefined
+var Y = 0; //sets y value to undefined
+
+function freeDrawing(free){
+    if (!isDrawing) return;
+
+    ctx.beginPath(); //starts a line
+    ctx.moveTo(X, Y); //starting point.needs user input to change the x and y cord.needs button to activate 
+    ctx.lineTo(free.offsetX, free.offsetY);//end point
+    ctx.stroke();//fills line
+
+}
+
+window.addEventListener("mousedown", (free) =>{//listens if mouse button is pressed and runs function freeDrawing
+    isDrawing = true; // changes var isDrawing to true
+    X = free.offsetX; // changes var X to free.offsetX (offsetX looks at where the mousepointer i in the x axis)
+    Y = free.offsetY; // changes var Y to free.offsetY (offsetY looks at where the mousepointer i in the y axis
+
+});
+
+window.addEventListener("mousemove", freeDrawing); // listening to mouse movment
+
+window.addEventListener("mouseup", (free) =>{ // listening if mouse button is relesed
+    isDrawing = false // sets var isDrawing to false
+})
+
+// Having a problem with the stroke not updating corectlly, it always draws from the starting point
 
 
 /*
