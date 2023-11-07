3aee656f6033b443f42c4bf7fb22f8b01adee593
Marcus Niklasson <85846673+MarcusNiklassonDen@users.noreply.github.com>
2023-11-07 18:16:19 +0100

    Fresh start
    removed the eventlisterners and started over

--- script.js
+++ script.js
@@ -5,53 +5,7 @@ canvas.height = window.innerHeight; //sets canvas height to window size
 
 var ctx = canvas.getContext("2d"); // dont really know what this dose but i guess it sets var ctx to be a 2d space?
 
-/*
-var mouse = { // creates mouse var with x and y cord
-    x: undefined, y: undefined 
-}
-*/
 
-
-
-window.addEventListener("mousemove", function(movment) {// listens for mousemovment cord
-    var x = movment.x;
-    var y = movment.y;
-    //mouse.x = movment.x; //changes undefined in mouse var to the event cord on x axis
-    //mouse.y = movment.y; //changes undefined in mouse var to the event cord on y axis
-
-});
-
-/*
-window.addEventListener("mousedown" , function(press) {// listens for mousemovment cord
-    var pressX = press.x;
-    var pressY = press.y;
-    //mouse.x = movment.x; //changes undefined in mouse var to the event cord on x axis
-    //mouse.y = movment.y; //changes undefined in mouse var to the event cord on y axis
-    
-   
-});
-
-window.addEventListener("mouseenter" , function(relese) {// listens for mousemovment cord
-    var releseX = relese.x;
-    var releseY = relese.y;
-    //mouse.x = movment.x; //changes undefined in mouse var to the event cord on x axis
-    //mouse.y = movment.y; //changes undefined in mouse var to the event cord on y axis
- 
-});
-
-ctx.beginPath(); //starts a line
-ctx.moveTo(press.x, press.y); //starting point.needs user input to change the x and y cord.needs button to activate 
-ctx.lineTo(releseX, releseY);//end point
-ctx.strokeStyle = "hexCode or css";// color select.needs input from user
-ctx.strokeStyle = "cap";
-ctx.stroke();//fills line
-
-//window.addEventListener("mousedown") // listens for mouse click
-
-
-//window.addEventListener("mouseup") //Listens for end of mouse click
-
-*/
 
 /*
 //SQUARE
