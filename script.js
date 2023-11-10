50c9e92f1aa030b1df09f7e1c6d1aa5a32ac64c3
Marcus Niklasson <85846673+MarcusNiklassonDen@users.noreply.github.com>
2023-11-10 15:50:09 +0100

    scraped the listers and functions
    made new functions and listeners thats a bit more logical to look at(for me anyways)

--- script.js
+++ script.js
@@ -3,61 +3,56 @@ var canvas = document.getElementById("canvas1");// looking for canvas tag in htm
 canvas.width = window.innerWidth; //sets canvas width to window size
 canvas.height = window.innerHeight; //sets canvas height to window size
 
-var ctx = canvas.getContext("2d"); // dont really know what this dose but i guess it sets var ctx to be a 2d space?
 
-ctx.strokeStyle = "black"; // sets color of stroke
-ctx.lineCap = "round" //sets end to be round
-ctx.lineWidth = 10; //sets width of stroke
-
+//variables
 var isDrawing = false; // sets variable isDrawning to boolian false
 var X  ; //storing x value
 var Y ; // storing y value
 var offsetX; //storing offsetX
 var offsetY; //storing offsetY
 var ctx //storing ctx
+var ctx = canvas.getContext("2d"); // dont really know what this dose but i guess it sets var ctx to be a 2d space?
+var strokeStyle = "black";//stores the strokeStyle (color)
+var lineWidth = "10";//stores the lineWidth (width of stroke)
 
 
-// Drawing function
-function freeDrawing(free){
-    if (!isDrawing) return;
+//sets the stroke atributes
+ctx.strokeStyle = strokeStyle; // sets color of stroke, refers to var strokeStyle
+ctx.lineCap = "round" //sets end to be round
+ctx.lineWidth = lineWidth; //sets width of stroke, refers to var lineWidth
 
-    ctx.beginPath(); //starts a line
-    ctx.moveTo(X, Y); //starting point.needs user input to change the x and y cord.needs button to activate 
-    ctx.lineTo(free.offsetX, free.offsetY);//end point
-    ctx.stroke();//fills line
 
+
+
+// Function that states that isDrawing is set to true
+function startDrawing(){
+    isDrawing = true;
 }
 
+// Function that states that isDrawing is set to false and the "Stroke Path" dosent continiue to the next StartDrawing function
+function stopDrawing(){
+    isDrawing = false
+    ctx.beginPath();
+}
 
-//Mouse Event Listeners
-window.addEventListener("mousemove", (free) =>{//listens if mouse  is moving
-    if (isDrawing) {
-        X = free.offsetX;// changes var X to free.offsetX (offsetX looks at where the mousepointer i in the x axis)
-        Y = free.offsetY;// changes var Y to free.offsetY (offsetY looks at where the mousepointer i in the y axis
-    }
+//Function for Mouse drawing (free drawing)
+function draw(freeDraw){
+    if (!isDrawing) return;// if isDrawing not true it runs the function again.
+    ctx.strokeStyle;// dont know if these are needed here or if it just needs the Style,lineCap,lineWidth from the top
+    ctx.lineCap;
+    ctx.lineWidth;
+
     
-
-});
-
-window.addEventListener("mousedown", (free) =>{//listens if mouse button is pressed and runs function freeDrawing
-    isDrawing = true; // changes var isDrawing to true
-    X = free.offsetX; // changes var X to free.offsetX (offsetX looks at where the mousepointer i in the x axis)
-    Y = free.offsetY; // changes var Y to free.offsetY (offsetY looks at where the mousepointer i in the y axis
+    ctx.lineTo(freeDraw.offsetX, freeDraw.offsetY);//takes cordinates from freeDraw thattakes them from draw that takes them fom mousemove
+    ctx.stroke();//fills the stroke (would like to have this last of the ctx but then it wount fill anything )
+    ctx.beginPath();//starts a new path (would like this on top but then i get dots instead of lines)
+    ctx.moveTo(freeDraw.offsetX, freeDraw.offsetY);//ends the stroke att these cordinates
     
-});
+}
 
-
-
-//window.addEventListener("mousemove", freeDrawing); // listening to mouse movment
-
-
-
-window.addEventListener("mouseup", (free) =>{ // listening if mouse button is relesed
-    isDrawing = false // sets var isDrawing to false
-})
-
-// Having a problem with the stroke not updating corectlly, it always draws from the starting point
-
+window.addEventListener("mousedown", startDrawing);//Lisening to mousebutton is pressed down and runs startDrawing function
+window.addEventListener("mouseup", stopDrawing);//listening to mousebutton is released and runs stopDrawing function
+window.addEventListener("mousemove", draw);//listening if mouse is moving and runs draw function
 
 /*
 //SQUARE
